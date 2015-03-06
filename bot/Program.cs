a939355fa095f4267c4d65ff02965f8f195c04e3
Rex Whitten <rwhitten@acrowire.com>
2015-03-06 14:29:54 -0500

    basic working version, with framework upgrades

---
+++ bot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bot
+{
+    using AIMLbot;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot myBot = new Bot();
+            myBot.loadSettings();
+            User myUser = new User("consoleUser", myBot);
+            myBot.isAcceptingUserInput = false;
+            myBot.loadAIMLFromFiles();
+            myBot.isAcceptingUserInput = true;
+            while (true)
+            {
+                Console.Write("You: ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                {
+                    break;
+                }
+                else
+                {
+                    Request r = new Request(input, myUser, myBot);
+                    Result res = myBot.Chat(r);
+                    Console.WriteLine("Bot: " + res.Output);
+                }
+            }
+        }
+    }
+}
